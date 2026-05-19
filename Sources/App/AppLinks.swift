84ca700cab1f5458c046be50ea67c86c3070352f
liuhuan086 <liuhuan086@gmail.com>
2026-05-19 17:18:52 +0800

    Point app links to public Pages site

--- Sources/App/AppLinks.swift
+++ Sources/App/AppLinks.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppLinks {
-    static let homepage = URL(string: "https://github.com/liuhuan086/swiftFTP")!
-    static let privacyPolicy = URL(string: "https://github.com/liuhuan086/swiftFTP/blob/main/Docs/Privacy.md")!
-    static let termsOfUse = URL(string: "https://github.com/liuhuan086/swiftFTP/blob/main/Docs/Terms.md")!
+    static let homepage = URL(string: "https://liuhuan086.github.io/app-pages/quay/")!
+    static let privacyPolicy = URL(string: "https://liuhuan086.github.io/app-pages/quay/privacy.html")!
+    static let termsOfUse = URL(string: "https://liuhuan086.github.io/app-pages/quay/terms.html")!
 }
